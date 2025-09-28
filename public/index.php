66caf437390ba132548cd885f347c8ad024ed393
Mike Zaspa <zaspamike0@gmail.com>
2025-09-28 19:52:51 +0800

    gadad

--- public/index.php
+++ public/index.php
@@ -1,18 +1,33 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__.'/../laravel_app/vendor/autoload.php';
+/*
 
-$app = require_once __DIR__.'/../laravel_app/bootstrap/app.php';
+*/
 
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+
+
+require __DIR__.'/../vendor/autoload.php';
+
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/** @var Kernel $kernel */
 $kernel = $app->make(Kernel::class);
 
-$response = tap($kernel->handle(
+$response = $kernel->handle(
     $request = Request::capture()
-))->send();
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
