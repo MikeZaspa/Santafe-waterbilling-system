7475adb8849777ec2637ec653a4ae31c304cc313
Mike Zaspa <zaspamike0@gmail.com>
2025-09-28 19:51:45 +0800

    hssfs

--- public/index.php
+++ public/index.php
@@ -1,33 +1,18 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
+require __DIR__.'/../laravel_app/vendor/autoload.php';
 
-*/
+$app = require_once __DIR__.'/../laravel_app/bootstrap/app.php';
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-
-
-require __DIR__.'/../vendor/autoload.php';
-
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-/** @var Kernel $kernel */
 $kernel = $app->make(Kernel::class);
 
-$response = $kernel->handle(
+$response = tap($kernel->handle(
     $request = Request::capture()
-);
-
-$response->send();
+))->send();
 
 $kernel->terminate($request, $response);
