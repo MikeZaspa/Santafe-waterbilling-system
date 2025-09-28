d028fc3494f5d268c45722f733986116ee713cb3
Mike Zaspa <zaspamike0@gmail.com>
2025-09-28 20:19:01 +0800

    dada

--- public/index.php
+++ public/index.php
@@ -1,16 +1,26 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if (file_exists($maintenance = __DIR__.'/../SANTAFEWATERBILLINGSYSTEM/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
 require __DIR__.'/../SANTAFEWATERBILLINGSYSTEM/vendor/autoload.php';
+
 $app = require_once __DIR__.'/../SANTAFEWATERBILLINGSYSTEM/bootstrap/app.php';
 
+/** @var Kernel $kernel */
 $kernel = $app->make(Kernel::class);
+
 $response = $kernel->handle(
     $request = Request::capture()
-)->send();
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
