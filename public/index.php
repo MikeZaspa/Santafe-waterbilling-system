4bc1cca5a419000f95bdd631ca3e89e2ddf23336
Mike Zaspa <zaspamike0@gmail.com>
2025-09-29 19:00:49 +0800

    gaag

--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+/*
 
+*/
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
@@ -13,5 +16,17 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 
 require __DIR__.'/../vendor/autoload.php';
+
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
