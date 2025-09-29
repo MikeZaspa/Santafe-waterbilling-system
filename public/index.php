a05695a68c87d759b3ca062c8fab110800895b85
MikeZaspa <112554920+MikeZaspa@users.noreply.github.com>
2025-09-29 11:10:36 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -21,3 +21,13 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
