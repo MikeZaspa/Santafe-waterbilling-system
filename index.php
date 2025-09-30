d113e494af7991074b29cd292e6491197aad914d
Mike Zaspa <zaspamike0@gmail.com>
2025-09-30 08:54:11 +0800

    dadad

--- index.php
+++ index.php
@@ -10,15 +10,15 @@ define('LARAVEL_START', microtime(true));
 
 */
 
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /** @var Kernel $kernel */
 $kernel = $app->make(Kernel::class);
