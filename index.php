ebf7a4b0d1c15d318c2a98b0d3a4650aaf22451a
Mike Zaspa <zaspamike0@gmail.com>
2025-09-30 09:09:03 +0800

    faad

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
 require __DIR__.'/vendor/autoload.php';
 
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /** @var Kernel $kernel */
 $kernel = $app->make(Kernel::class);
