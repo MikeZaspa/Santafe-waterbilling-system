625997f3aa7650c5d078fa086adf5549a75f09de
Mike Zaspa <zaspamike0@gmail.com>
2025-09-30 10:13:25 +0800

    adada

--- public/index.php
+++ public/index.php
@@ -1,56 +1,24 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
 /*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command,
-| we will require this file so that any pre-rendered content can be shown
-| instead of starting the framework.
-|
+
 */
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader
-| for this application. We just need to utilize it! We'll simply require
-| it into the script here so we don't have to worry about manual loading.
-|
-*/
-
+//Register the composer autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request through
-| the kernel, and send the associated response back to the client's browser
-| allowing them to enjoy the creative and wonderful application we have prepared.
-|
-*/
+// Bootstrap Laravel and handle the request...
 
+  /**  @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
