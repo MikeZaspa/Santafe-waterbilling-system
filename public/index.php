e664a3f707c08050160fb5fa5e927f334114a136
Mike Zaspa <zaspamike0@gmail.com>
2025-09-30 09:29:37 +0800

    fafadada

--- public/index.php
+++ public/index.php
@@ -1,24 +1,24 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+
+/*
+
+*/
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
+//Register the composer autoloader
 require __DIR__.'/../vendor/autoload.php';
 
+// Bootstrap Laravel and handle the request...
+
+  /**  @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-/** @var Kernel $kernel */
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
