a1d01c4da8a85f69368aef3bef4cb014528a1065
Mike Zaspa <zaspamike0@gmail.com>
2025-10-01 20:29:28 +0800

    dadada

--- public/index.php
+++ public/index.php
@@ -1,24 +1,56 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
 /*
-
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
 */
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-//Register the composer autoloader
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for this application. We just need to utilize it! We'll simply require
+| it into the script here so we don’t need to manually load our classes.
+|
+*/
+
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+/*
+|--------------------------------------------------------------------------
+| Bootstrap The Application
+|--------------------------------------------------------------------------
+|
+| Here we will bootstrap the Laravel application and run it, capturing
+| the incoming request and then sending the associated response back
+| to the client’s browser for them to enjoy a great experience.
+|
+*/
 
-  /**  @var Application $app */
+/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$app->withMiddleware(function (Middleware $middleware) {
+    //
+})->withExceptions(function (Exceptions $exceptions) {
+    //
+})->handleRequest(Request::capture());
