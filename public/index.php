219a940930c81f762da9c0d3fbe3d62b6e6e7f54
Mike Zaspa <zaspamike0@gmail.com>
2025-10-01 20:35:04 +0800

    ahaafa

--- public/index.php
+++ public/index.php
@@ -1,24 +1,23 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
-/*
-
-*/
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-//Register the composer autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-
-  /**  @var Application $app */
+/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$app->withMiddleware(function (Middleware $middleware) {
+    //
+})->withExceptions(function (Exceptions $exceptions) {
+    //
+})->handleRequest(Request::capture());
