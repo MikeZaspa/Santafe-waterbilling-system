d47a7485bf184bfde8d60810c11124acd425e6da
Mike Zaspa <zaspamike0@gmail.com>
2025-11-13 15:37:04 +0800

    sasa

--- app/Models/Plumber.php
+++ app/Models/Plumber.php
@@ -3,13 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable; // <-- 1. I-import kini
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Notifications\Notifiable; // <-- 2. I-import kini (maayo nga i-add)
 
-class Plumber extends Authenticatable // <-- 3. I-extend kini diri
+class Plumber extends Model
 {
-    use HasFactory, SoftDeletes, Notifiable; // <-- 4. Dugang ang Notifiable trait
+    use HasFactory, SoftDeletes;
 
     // Specify the table name explicitly
     protected $table = 'admin_plumbers';
@@ -39,4 +38,5 @@ class Plumber extends Authenticatable // <-- 3. I-extend kini diri
         'email_verified_at' => 'datetime',
         'two_factor_expires_at' => 'datetime',
     ];
+
 }
