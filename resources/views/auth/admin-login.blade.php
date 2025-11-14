d243e65af3f6cfa03e09e3759b893c1d791316cc
Mike Zaspa <zaspamike0@gmail.com>
2025-11-14 09:46:48 +0800

    sasa

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -467,18 +467,6 @@
         <div class="modal fade forgot-password-modal" id="forgotPasswordModal" tabindex="-1" aria-labelledby="forgotPasswordModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <div class="logo">
-                            <!-- Icon from the design -->
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#111"><path d="M3 3h3v3H3V3zm4 0h3v3H7V3zm4 0h3v3h-3V3zm4 0h3v3h-3V3zm-8 4h3v3H7V7zm4 0h3v3h-3V7zm4 0h3v3h-3V7zm-8 4h3v3H7v-3zm4 0h3v3h-3v-3zm4 0h3v3h-3v-3zm-8 4h3v3H7v-3zm4 0h3v3h-3v-3zm4 0h3v3h-3v-3z"/></svg>
-                            Spark
-                        </div>
-                        <nav>
-                            <a href="#">About</a>
-                            <a href="#">FAQ</a>
-                            <a href="#">Docs</a>
-                        </nav>
-                    </div>
                     <div class="modal-body">
                         <h1>Forgot Password</h1>
                         <form id="forgotPasswordForm">
