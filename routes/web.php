f5f02684b788c7ecf3e966408ab6f91de56b3099
Mike Zaspa <zaspamike0@gmail.com>
2025-11-15 08:29:32 +0800

    dada

--- routes/web.php
+++ routes/web.php
@@ -459,13 +459,8 @@ Route::get('/admin/plumbers/{id}/edit', [PlumberController::class, 'edit'])->nam
 Route::put('/admin/plumbers/{id}', [PlumberController::class, 'update'])->name('admin.plumbers.update');
 Route::delete('/admin/plumbers/{id}', [PlumberController::class, 'destroy'])->name('admin.plumbers.destroy');
 
-// Protected plumber routes using a Closure
-Route::middleware(function ($request, $next) {
-    if (!Session::get('plumber_logged_in')) {
-        return redirect('/plumber-login')->with('error', 'Please login to access this page.');
-    }
-    return $next($request);
-})->group(function () {
+// Protected plumber routes using the CLASS-BASED ALIAS
+Route::middleware(['plumber.auth'])->group(function () {
     Route::get('/admin-plumber-dashboard', [ReadingController::class, 'index'])->name('admin.plumber.dashboard');
     Route::get('/dashboard-data', [ReadingController::class, 'getDashboardData']);
     Route::get('/reconnected-consumers', [ReadingController::class, 'getReconnectedConsumers']);
