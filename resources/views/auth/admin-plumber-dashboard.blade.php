f45502a8ae07351991b3d437617cf2fe4d107471
Mike Zaspa <zaspamike0@gmail.com>
2025-11-17 06:20:47 +0800

    fafafa

--- resources/views/auth/admin-plumber-dashboard.blade.php
+++ resources/views/auth/admin-plumber-dashboard.blade.php
@@ -563,8 +563,6 @@
                     <span class="d-none d-md-inline">Plumber</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
-                    <li><a class="dropdown-item" href="#"><i class="bi bi-person me-2"></i>Profile</a></li>
-                    <li><a class="dropdown-item" href="#"><i class="bi bi-gear me-2"></i>Settings</a></li>
                     <li><hr class="dropdown-divider"></li>
                     <li>
                         <a class="dropdown-item text-danger" href="#" id="logoutBtn">
